bfb500233d722fa9382d3c241deb401b84b60b3c
Anne-LieStahle85 <149506235+Anne-LieStahle85@users.noreply.github.com>
2023-12-12 14:01:25 +0100

    added files

---
+++ script.js
@@ -0,0 +1,69 @@
+const countryInput = document.getElementById("countryInput");
+const countryInfo = document.getElementById("countryInfo");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getCountriesByChoice();
+  form.reset();
+});
+
+async function getCountriesByChoice() {
+  const path = document.querySelector(":checked").value;
+  const userChoice = document.querySelector("input").value;
+  const url = `https://restcountries.com/v3.1/${path}/${userChoice}?fields=capital,population,subregion,name,flags`;
+
+  try {
+    const response = await fetch(url);
+    console.log(response);
+
+    const data = await response.json();
+    console.log(data);
+    
+    const containerDiv = document.querySelector("#countryInfo");
+    containerDiv.innerHTML = "";
+
+    if (response.ok) {
+      data.sort((a, b) => b.population - a.population);
+      data.forEach( createCountryDiv );
+    } else if (response.status === 404) {
+      console.log(404);
+      containerDiv.innerHTML = "<p>No picture was found</p>";
+    } else {
+      containerDiv.innerHTML = "<p>Something went wrong, try again.";
+    }
+  } catch (error) {
+    displayError(error);
+  }
+}
+
+function createCountryDiv(countryData) {
+  const containerDiv = document.querySelector("#countryInfo");
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("country-container");
+
+  const flagImg = document.createElement("img");
+  flagImg.src = countryData.flags.png;
+
+  const countryName = document.createElement("h2");
+  countryName.innerText = countryData.name.common;
+
+  const capitalInfo = document.createElement("p");
+  capitalInfo.innerText = `Capital: ${countryData.capital}`;
+
+  const populationInfo = document.createElement("p");
+  populationInfo.innerText = `Population: ${countryData.population}`;
+
+  const subregionInfo = document.createElement("p");
+  subregionInfo.innerText = `Subregion: ${countryData.subregion}`;
+
+  newDiv.append(flagImg, countryName, capitalInfo, populationInfo, subregionInfo);
+
+  containerDiv.appendChild(newDiv);
+}
+
+function displayError(error) {
+  console.error("Error", error);
+  const containerDiv = document.querySelector("#countryInfo");
+  containerDiv.innerHTML = "<p>Error fetching country data. Try again.</p>";
+}
